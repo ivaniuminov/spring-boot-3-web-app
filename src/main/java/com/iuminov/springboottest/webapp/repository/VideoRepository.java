fdf2d58774c2106f288e9798e8a74438a078eea7
ivaniuminov <ivan.iuminov@gmail.com>
2024-11-08 23:46:27 +0100

    Added H2 config

--- src/main/java/com/iuminov/springboottest/webapp/repository/VideoRepository.java
+++ src/main/java/com/iuminov/springboottest/webapp/repository/VideoRepository.java
@@ -2,11 +2,19 @@ package com.iuminov.springboottest.webapp.repository;
 
 import com.iuminov.springboottest.webapp.entity.VideoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface VideoRepository extends JpaRepository<VideoEntity, Long> {
     List<VideoEntity> findByName(String name);
+
+    @Query("select v from VideoEntity v where v.name = ?1")
+    List<VideoEntity> findCustomerReport(String name);
+
+    @Query(value="select * from VIDEO_ENTITY where NAME = ?1",
+            nativeQuery=true)
+    List<VideoEntity> findCustomWithPureSql(String name);
 
     List<VideoEntity> findByNameContainsOrDescriptionContainsAllIgnoreCase(String name, String description);
 
